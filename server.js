452227dbcf6b4770d328e7fe9dff9d5ccea7154a
zachary daltorio <zachary.d@zdaltorio.me>
2019-10-14 04:22:47 -0400

    Major Refactor to improve readablity and keeping code clean, started work on theming
    Signed-off-by: zachary daltorio <zachary.d@zdaltorio.me>

--- server.js
+++ server.js
@@ -69,7 +69,6 @@ passport.use(new local(
     r.db(config.DB_Name).table('users').filter(r.row('username').eq(username)).run(connection, function (err, user) {
       if (err) { return done(err); }
       if (!user) { return done(null, false); }
-
       user.toArray(function(err, result) {
           if (err) throw err;
           if (result.length == 0) { return done(null, false); }
@@ -92,123 +91,111 @@ passport.deserializeUser(function (id, done) {
     });
   });
 });
-
-
 //Site
 // welcome page
 app.get('/', function (req, res) {
-  active = "Home";
-  let displayname;
-  let profile;
   if(req.isAuthenticated()){
-    displayname=req.user.username;
-    let profile;
-    profile = user_model(req.user.id);
-    id = req.user.id;
+  page_processer(req.user.id,req.isAuthenticated(), "Home", "index", "index", "home", "null", req, res)
   }
   else{
-    displayname="null";
-    id = "null";
+  page_processer(null,req.isAuthenticated(), "Home", "index", "index", "home", "null", req, res)
   }
-  res.render('template', {username: displayname,profile_pic: profile,authed: req.isAuthenticated(), content:'index', path:'home', result: null, error: null, active: active});
 });
-
 
 // login and register
+
 app.get('/login', loggedOut, function (req, res) {
-  active = "Account";
-  let displayname;
-  displayname="null";
-  res.render('template', {authed: req.isAuthenticated(), content:'login', path:'home', result: null, error: null, active: active});
-});
-
-app.get('/register', loggedOut, function (req, res) {
-  active = "Home";
-  let displayname;
-  displayname="null";
-  res.render('template', {authed: req.isAuthenticated(), content:'register', path:'home', username: displayname, result: null, error: null, active: active});
-});
-
-
-// home
-app.get('/home/:page', loggedIn, function (req, res) {
-  active = "Home";
-  profile = user_model(req.user.id);
-  res.render('template', {authed: req.isAuthenticated(), content:req.params.page, path:'home', username: req.user.username, id: req.user.id, result: null, error: null, active: active});
-});
-
-app.get('/account', loggedIn, function (req, res) {
-  console.log(req.user.id)
-  active = "Account";
-  profile = user_model(req.user.id);
-  res.render('template', {authed: req.isAuthenticated(), content:'index', path:'account', username: req.user.username, id: req.user.id, result: null, error: null, active: active});
-});
-
-app.get('/about', function (req, res) {
-  active = "About";
-  let displayname;
   if(req.isAuthenticated()){
-    displayname=req.user.username;
-    profile = user_model(req.user.id);
+  page_processer(req.user.id,req.isAuthenticated(), "Account", "login", "index", "home", "null", req, res)
   }
   else{
-    displayname="null";
+  page_processer(null,req.isAuthenticated(), "Account", "login", "index", "home", "null", req, res)
+  }});
+
+app.get('/register', loggedOut, function (req, res) {
+  if(req.isAuthenticated()){
+  page_processer(req.user.id,req.isAuthenticated(), "Account", "register", "index", "home", "null", req, res)
   }
-  res.render('template', {authed: req.isAuthenticated(), content:'index', path:'about',username: displayname, result: null, error: null, active: active});
-});
+  else{
+  page_processer(null,req.isAuthenticated(), "Account", "register", "index", "home", "null", req, res)
+  }});
+// home
 
+app.get('/home/:page', loggedIn, function (req, res) {
+  if(req.isAuthenticated()){
+  page_processer(req.user.id,req.isAuthenticated(), "home", req.params.page, "index", "home", "null", req, res)
+  }
+  else{
+  page_processer(null,req.isAuthenticated(), "home", req.params.page, "index", "home", "null", req, res)
+  }});
 
+app.get('/account', loggedIn, function (req, res) {
+  if(req.isAuthenticated()){
+  page_processer(req.user.id ,req.isAuthenticated(), "Account", "index", "index", "account", "null", req, res)
+  }
+  else{
+  page_processer(null ,req.isAuthenticated(), "Account", "index", "index", "account", "null", req, res)
+  }});
+
+app.get('/about', function (req, res) {
+  if(req.isAuthenticated()){
+  page_processer(req.user.id,req.isAuthenticated(), "About", "index", "index", "about", "null", req, res)
+  }
+  else{
+  page_processer(null,req.isAuthenticated(), "About", "index", "index", "about", "null", req, res)
+  }});
 // Settings
+
 app.get('/settings', loggedIn, function (req, res) {
-  active = "Settings";
-  profile = user_model(req.user.id);
-  res.render('template', {authed: req.isAuthenticated(), content:'index', path:'settings', username: req.user.username, id: req.user.id, result: null, error: null, active: active});
-});
+  if(req.isAuthenticated()){
+  page_processer(req.user.id,req.isAuthenticated(), "Settings", "index", "index", "settings", "null", req, res)
+  }
+  else{
+  page_processer(null,req.isAuthenticated(), "Settings", "index", "index", "settings", "null", req, res)
+  }});
+
 app.get('/settings/:page', loggedIn, function (req, res) {
-  active = "Settings";
-  profile = user_model(req.user.id);
-  res.render('template', {authed: req.isAuthenticated(), content:req.params.page, path:'settings', username: req.user.username, id: req.user.id, result: null, error: null, active: active});
-});
-
-
+  if(req.isAuthenticated()){
+  page_processer(req.user.id,req.isAuthenticated(), "Settings", "index", "index", "settings", "null", req, res)
+  }
+  else{
+  page_processer(null,req.isAuthenticated(), "Settings", req.params.page, "index", "settings", "null", req, res)
+  }});
 // dashboard
+
 app.get('/dashboard', loggedIn, function (req, res) {
-  active = "Dashboard";
-  profile = user_model(req.user.id);
-  let list = list_all(req.user.id).then(data => {
-    let Filtered_data = data;
-    console.log("wat");
-    res.render('template', {authed: req.isAuthenticated(), content:'index', page:'overview', path:'dashboard', username: req.user.username, id: req.user.id, result: Filtered_data, error: null, active: active});    }).catch(err => {
-  });;
-});
+  if(req.isAuthenticated()){
+  page_processer(req.user.id,req.isAuthenticated(), "Dashboard", "index", "overview", "dashboard", "null", req, res)
+  }
+  else{
+  page_processer(null,req.isAuthenticated(), "Dashboard", "index", "overview", "dashboard", "null", req, res)
+  }});
+
 app.get('/dashboard/:page', loggedIn, function (req, res) {
-  active = "Dashboard";
-  profile = user_model(req.user.id);
-  res.render('template', {authed: req.isAuthenticated(), content:'index', page:req.params.page, path:'dashboard', username: req.user.username, id: req.user.id, result: null, error: null, active: active});
-});
-
-
+  if(req.isAuthenticated()){
+  page_processer(req.user.id,req.isAuthenticated(), "Dashboard", "index", req.params.page, "dashboard", "null", req, res)
+  }
+  else{
+  page_processer(null,req.isAuthenticated(), "Dashboard", "index", req.params.page, "dashboard", "null", req, res)
+  }});
 //Instance Page
+
 app.get('/Server/:Server_ID', loggedIn, function (req, res) {
-  active = "Dashboard";
-  profile = user_model(req.user.id);
-  let list = list_all(req.user.id).then(data => {
-    let Filtered_data = data;
-    console.log("wat");
-    res.render('template', {authed: req.isAuthenticated(), content:'server_page', path:'Server', username: req.user.username, id: req.user.id, result: Filtered_data, error: null, active: active});    }).catch(err => {
-  });;
-});
-
-
+  if(req.isAuthenticated()){
+  page_processer(req.user.id,req.isAuthenticated(), "Dashboard", "server_page", req.params.page, "server", req.params.page, req, res)
+  }
+  else{
+  page_processer(null,req.isAuthenticated(), "Dashboard", "server_page", req.params.page, "server", req.params.page, req, res)
+  }});
 // logout
+
 app.get('/logout', function(req, res){
   req.logout();
   res.redirect('/');
 });
-
-
 // APP Posts
 //user
+
 app.post('/login',
   passport.authenticate('local', { failureRedirect: '/login',
       failureFlash: true }), function(req, res) {
@@ -219,6 +206,7 @@ app.post('/login',
         }
       res.redirect('/dashboard');
 });
+
 app.post('/register', function (req, res) {
   let newusername = req.body.newusername;
   let newpassword = req.body.newpassword
@@ -234,16 +222,18 @@ app.post('/register', function (req, res) {
   }
   res.render('template', {authed: req.isAuthenticated(), content:'register', path:'home', username: displayname, result: null, error: null, active: active});
 });
-
 // server
+
 app.post('/submit', function (req, res) {
   let send = query();
   res.render('index', {result: null, error:null});
 });
+
 app.post('/update/:Server_ID', function (req, res) {
   let send = update();
   res.render('index', {result: null, error:null});
 });
+
 app.post('/create_Server', function (req,res) {
   let game = req.body.Game;
   let slots = req.body.Slots;
@@ -257,31 +247,47 @@ app.post('/create_Server', function (req,res) {
   let send = query(game, slots, ip, hostname, name, owner, ram, storage, backup);
   res.redirect('/dashboard');
 })
-
-
 //API
-
-
 //Gets
-
-
-
 //Posts
-
-
 // error pages
 
 app.use(function(req, res) {
   res.status(404);
   res.render('template', {authed: req.isAuthenticated(), content:'404', path:'errors', username: displayname, result: null, error: null, active: 'null'});
 });
+
 app.use(function(error, req, res, next) {
   console.log(error);
   res.status(500);
   res.render('template', {authed: req.isAuthenticated(), content:'500', path:'errors', username: displayname, result: null, error: null, active: 'null'});
 });
-
 // Functions
+
+function page_processer(userid ,auth , active, passed_content, passed_page, passed_path, serverid, req, res){
+  if(auth){
+    console.log("authed");
+    let lookup = data_model("users", userid).then(data => {
+    let user_obj = JSON.parse(data);
+    let list = list_servers(req.user.id).then(data => {
+    let Filtered_data = data;
+    console.log("wat");
+    res.render('template', {authed: auth, content: passed_content, page: passed_page, path: passed_path, user: user_obj, result: Filtered_data, active: active});
+    }).catch(err => {
+    });
+  })}
+  else{
+    console.log("not authed");
+    let db_object;
+    let stringed_object;
+    db_object = {theme:"/css/dark.css"};
+    stringed_object = JSON.parse(JSON.stringify(db_object, null, 2))
+    let id = "null";
+    let Filtered_data = "null";
+    let displayname= "null";
+    let profile = "../images/avatars/" + id + ".png";
+    res.render('template', {authed: auth, content: passed_content, page: passed_page, path: passed_path, user: stringed_object, result: Filtered_data, active: active});
+  }};
 
 function query(game, slots, ip, hostname, name, owner, ram, storage, backup){
   return new Promise ( (resolve, reject) => {
@@ -303,39 +309,72 @@ function query(game, slots, ip, hostname, name, owner, ram, storage, backup){
   });
   })};
 
-
-function list_all(id){
+function list_servers(id){
   return new Promise ( (resolve, reject) => {
     r.table('servers').filter(function(row) {
   return row('permissions').contains(id);
 }).run(connection, function(err, cursor) {
-      var lookup = cursor.toArray();
+      let lookup = cursor.toArray();
       console.log(lookup);
       return resolve(lookup);
     });    
 })};
 
-function user_model(id){
-  let profile;
-  profile = "../images/avatars/" + id + ".png";
-  return profile;
-}
+function server_model(user, server){
+  return new Promise ( (resolve, reject) => {
+  r.table('servers').filter('"' + server + '"').run(connection, function(err, cursor) {
+  let db_server = JSON.stringify(cursor, null, 2);
+  if (req.isAuthenticated()) return next();
+  console.log(req.session);
+  if (req.method == 'GET') req.session.returnTo = req.originalUrl;
+  res.redirect('/');
+  console.log(db_server);
+  return resolve(db_server);
+    });    
+})};
 
+function data_model(table, id){
+  return new Promise ( (resolve, reject) => {
+  console.log(table +":"+ id);
+  r.table(table).get(id).run(connection, function(err, cursor) {
+    if (err) throw err;
+    let db_object = cursor;
+    console.log(db_object);
+    if (table == "users"){
+      let profile;
+      console.log("I'm a user");
+      value = "../images/avatars/" + id + ".png";
+      db_object['profile'] = value;
+      stringed_object = JSON.stringify(db_object, null, 2)
+      console.log(stringed_object);
+      return resolve(stringed_object);
+    } 
+    else if (table == "servers"){
+      console.log(db_object);
+      return resolve(db_object);
+    }
+    else
+      console.log(db_object);
+      return resolve(db_object); 
+    });    
+})};
 // User
-
 // Authentication
-function ensureAuthenticated(req, res, next) {
+
+function ensureAuthenticated(req, res) {
   if (req.isAuthenticated()) return next();
   console.log(req.session);
   if (req.method == 'GET') req.session.returnTo = req.originalUrl;
   res.redirect('/');
 }
+
 function ensureUnauthenticated(req, res, next) {
   if (!req.isAuthenticated()) return next();
   console.log(req.session);
   if (req.method == 'GET') req.session.returnTo = req.originalUrl;
   res.redirect('/');
 }
+
 function loggedIn(req, res, next) {
     if (req.user) {
         next();
@@ -343,6 +382,7 @@ function loggedIn(req, res, next) {
         res.redirect('/login');
     }
 }
+
 function loggedOut(req, res, next) {
     if (req.user) {
       console.log("no user");
@@ -351,6 +391,7 @@ function loggedOut(req, res, next) {
       next();
     }
 }
+
 function nameCheck(req, res) {
 if (req.isAuthenticated()) return req.user.username;
 else {
@@ -366,16 +407,17 @@ function adduser(newusername, newpassword) {
              r.db(config.DB_Name).table('users').insert({
             username: newusername,
             password: bcrypt.hashSync(newpassword),
-            roll: 0
+            roll: 0,
+            theme: "dark"
           }).run(connection, function(err, res) {
             console.log('Created default user: newusername');
           });
     })
 }
-function displayname(username){
-let displayname = username;
-let defaultPhrase = "Unknown User";
-let phrase = (typeof displayname === "undefined" ? defaultPhrase : displayname);
-return displayname;
 
+function displayname(username){
+  let displayname = username;
+  let defaultPhrase = "Unknown User";
+  let phrase = (typeof displayname === "undefined" ? defaultPhrase : displayname);
+  return displayname;
 }
